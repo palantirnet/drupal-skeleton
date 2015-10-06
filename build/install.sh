ea2f2ce2fe579a99e7469a23f8f73451f71cf6e1
Arthur Foelsche <foelsche@palantir.net>
2015-10-06 14:12:18 -0400

    Updating the paths

--- build/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-path=$(dirname "$0")
-source $path/common.sh
-
-echo "Installing Drupal minimal profile.";
-$drush si minimal --site-name=skeleton --account-name=admin --account-pass=admin
-source $path/update.sh
