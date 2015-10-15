e2756a55e2ef694d6f2860d5b2e17fe9f3274eb9
Arthur Foelsche <foelsche@palantir.net>
2015-10-15 11:24:38 -0400

    Removes the sitespeed install on the vagrant install to fix the permission and lack of java issue

--- build/install.sh
+++ build/install.sh
@@ -3,8 +3,6 @@ set -e
 path=$(dirname "$0")
 source $path/common.sh
 
-npm install -g sitespeed.io
-
 echo "Installing Drupal minimal profile.";
 $drush si minimal --site-name=skeleton --account-name=admin --account-pass=admin
 source $path/update.sh
