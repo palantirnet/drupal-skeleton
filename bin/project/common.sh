40103fd6e0083d4e2842c63a1dbe98d3ef651c90
Arthur Foelsche <foelsche@palantir.net>
2015-11-30 13:17:10 -0500

    Further updates for dealing with the pear.php outage

--- bin/project/common.sh
+++ bin/project/common.sh
@@ -3,7 +3,7 @@
 set -e
 path=$(dirname "$0")
 base=$(cd $path/../.. && pwd)
-drush="$base/bin/drush.php $drush_flags -y -r $base/www"
+drush="$base/bin/drush $drush_flags -y -r $base/www"
 
 if [[ -f .env ]]; then
   source .env
