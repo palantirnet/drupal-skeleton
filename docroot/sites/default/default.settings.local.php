cf35633f545306d9e9291aac4fed85cddbd4c7b5
Bec White <white@palantir.net>
2021-09-30 10:36:34 -0500

    Add a line in the ddev-override settings file for overriding the solr index config.

--- docroot/sites/default/default.settings.local.php
+++ docroot/sites/default/default.settings.local.php
@@ -7,3 +7,7 @@
  * Copy this file to 'settings.local.php' and customize for your development needs.
  */
 
+// To simulate other config split environments, change "development" to either "staging"
+// or "production", then run:
+//   drush cr && drush cim -y
+# $config['config_split.config_split.development']['status'] = TRUE;
