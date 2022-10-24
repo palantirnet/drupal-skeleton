04159a98237edbfe3507dc77544424ab71739294
Bec White <white@palantir.net>
2022-10-24 15:16:17 -0500

    Move dev caching settings to settings.local.php and services.local.yml to make them easier to turn on and off.

--- docroot/sites/default/default.settings.local.php
+++ docroot/sites/default/default.settings.local.php
@@ -7,7 +7,19 @@
  * Copy this file to 'settings.local.php' and customize for your development needs.
  */
 
-// To simulate other config split environments, change "development" to either "staging"
-// or "production", then run:
+// To simulate other config split environments, uncomment these split lines, then run:
 //   drush cr && drush cim -y
-# $config['config_split.config_split.development']['status'] = TRUE;
+# $config['config_split.config_split.development']['status'] = FALSE;
+# $config['config_split.config_split.production']['status'] = TRUE;
+
+// Customizable development services file.
+$settings['container_yamls'][] = DRUPAL_ROOT . '/' . $site_path . '/services.local.yml';
+
+// Disable caching.
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+// Disable CSS and JS aggregation.
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
